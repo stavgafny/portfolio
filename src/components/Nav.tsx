182a3abf51ca023e824ad6b3effc1bd0897f44c7
stavgafny <stavgafny@gmail.com>
2023-06-30 18:59:43 +0300

    responsive nav, hamburger menu on smaller screens

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,34 +6,53 @@ import { GoHome, GoPerson } from 'react-icons/go'
 import { BsCode } from 'react-icons/bs'
 import { HiOutlineDocumentText } from 'react-icons/hi'
 import { FiGithub } from 'react-icons/fi'
+import { RxHamburgerMenu } from 'react-icons/rx'
 
 export default function Nav () {
   return (
-    <nav className='flex items-center justify-items-end flex-wrap p-3'>
-      <div className='w-full flex justify-center gap-x-16'>
-          <NavItem href='/' title='Home' icon={<GoHome className='scale-150' />} />
-          <NavItem href='/' title='About' icon={<GoPerson className='scale-150' />} />
-          <NavItem href='/' title='Projects' icon={<BsCode className='scale-150' />} />
-          <div className='flex gap-8'>
-          <NavShortcut href='/' title='Resume' icon={<HiOutlineDocumentText className='scale-150' style={{color: "#8af"}} />} />
-          <NavShortcut href='/' title='Github' icon={<FiGithub className='scale-150' style={{color: "#a7f"}} />} />
-          </div>
-
-          <div className='sm:hidden'>
-            <button>ham</button>
-          </div>
+    <nav className='flex items-center justify-end flex-wrap p-3'>
+      <div className='md:hidden'>
+        <button>
+          <RxHamburgerMenu className='scale-150' />
+        </button>
+      </div>
+      <div className='w-full flex justify-center gap-x-16 max-md:hidden'>
+        <NavItem
+          href='/'
+          title='Home'
+          icon={<GoHome className='scale-150 text-pink-300' />}
+        />
+        <NavItem
+          href='/'
+          title='About'
+          icon={<GoPerson className='scale-150 text-emerald-400' />}
+        />
+        <NavItem
+          href='/'
+          title='Projects'
+          icon={<BsCode className='scale-150 text-yellow-500' />}
+        />
+        <NavItem
+          href='/'
+          title='Resume'
+          icon={<HiOutlineDocumentText className='scale-150 text-blue-300' />}
+        />
+        <NavItem
+          href='/'
+          title='Github'
+          icon={<FiGithub className='scale-150 text-purple-400' />}
+        />
       </div>
     </nav>
   )
 }
 
 interface NavItemProps {
-    href: Url
-    title: string
-    icon: ReactNode
+  href: Url
+  title: string
+  icon: ReactNode
 }
 
-  
 function NavItem ({ href, title, icon }: NavItemProps) {
   return (
     <Link href={href} title={title} className='flex items-center gap-4'>
@@ -44,9 +63,9 @@ function NavItem ({ href, title, icon }: NavItemProps) {
 }
 
 function NavShortcut ({ href, title, icon }: NavItemProps) {
-    return (
-      <Link href={href} title={title} className='flex items-center gap-4'>
-        {icon}
-      </Link>
-    )
-  }
+  return (
+    <Link href={href} title={title} className='flex items-center gap-4'>
+      {icon}
+    </Link>
+  )
+}
