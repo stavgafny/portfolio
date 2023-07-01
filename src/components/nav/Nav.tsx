ff00cb958a0a1083af776f91abd5bba9eb159e68
stavgafny <stavgafny@gmail.com>
2023-07-02 02:19:08 +0300

    cleaning and removing redundant nav items

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -20,72 +20,52 @@ export default function Nav () {
         <HamburgerMenu opened={opened} onTap={handleClick} />
       </div>
       <div
-        className={`nav_content justify-around items-center ${opened ? 'nav_opened' : ''} w-full lg:hidden`}
+        className={`nav_content justify-around items-center ${
+          opened ? 'nav_opened' : ''
+        } w-full lg:hidden`}
       >
-        <NavItem
-          href='/'
-          title='Home'
-          icon={<GoHome className='scale-150 text-red-300' />}
-        />
-        <NavItem
-          href='/'
-          title='About'
-          icon={<GoPerson className='scale-150 text-emerald-400' />}
-        />
-        <NavItem
-          href='/'
-          title='Skills'
-          icon={<LiaCubesSolid className='scale-150 text-orange-300' />}
-        />
-        <NavItem
-          href='/'
-          title='Projects'
-          icon={<BsCode className='scale-150 text-yellow-300' />}
-        />
-        <NavItem
-          href='/'
-          title='Resume'
-          icon={<HiOutlineDocumentText className='scale-150 text-blue-300' />}
-        />
-        <NavItem
-          href='/'
-          title='Github'
-          icon={<FiGithub className='scale-150 text-purple-400' />}
-        />
+        <_NavItems />
       </div>
-
       <div className='w-full flex justify-center gap-x-16 max-lg:hidden'>
-        <NavItem
-          href='/'
-          title='Home'
-          icon={<GoHome className='scale-150 text-red-300' />}
-        />
-        <NavItem
-          href='/'
-          title='About'
-          icon={<GoPerson className='scale-150 text-emerald-400' />}
-        />
-        <NavItem
-          href='/'
-          title='Skills'
-          icon={<LiaCubesSolid className='scale-150 text-orange-300' />}
-        />
-        <NavItem
-          href='/'
-          title='Projects'
-          icon={<BsCode className='scale-150 text-yellow-300' />}
-        />
-        <NavItem
-          href='/'
-          title='Resume'
-          icon={<HiOutlineDocumentText className='scale-150 text-blue-300' />}
-        />
-        <NavItem
-          href='/'
-          title='Github'
-          icon={<FiGithub className='scale-150 text-purple-400' />}
-        />
+        <_NavItems />
       </div>
     </nav>
+  )
+}
+
+const _NavItems = () => {
+  return (
+    <>
+      <NavItem
+        href='/'
+        title='Home'
+        icon={<GoHome className='scale-150 text-red-300' />}
+      />
+      <NavItem
+        href='/'
+        title='About'
+        icon={<GoPerson className='scale-150 text-emerald-400' />}
+      />
+      <NavItem
+        href='/'
+        title='Skills'
+        icon={<LiaCubesSolid className='scale-150 text-orange-300' />}
+      />
+      <NavItem
+        href='/'
+        title='Projects'
+        icon={<BsCode className='scale-150 text-yellow-300' />}
+      />
+      <NavItem
+        href='/'
+        title='Resume'
+        icon={<HiOutlineDocumentText className='scale-150 text-blue-300' />}
+      />
+      <NavItem
+        href='/'
+        title='Github'
+        icon={<FiGithub className='scale-150 text-purple-400' />}
+      />
+    </>
   )
 }
