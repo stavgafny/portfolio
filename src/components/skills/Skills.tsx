4d453d6bc1a02d423cbc3dddbbe8d94985606006
stavgafny <stavgafny@gmail.com>
2023-07-13 01:14:25 +0300

    adding skills boxes with their proper box sections adding paragraph tag effect on box section

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -13,22 +13,99 @@ export default function Skills () {
           </AnimateOnScrollObserver>
         </div>
 
-        <div className={styles.skill_boxes + ' flex max-lg:flex-col'}>
-          <SkillBox title='Software Development' markerColor='bg-pink-400' />
-          <SkillBox title='Frontend Dev' markerColor='bg-blue-400' />
-          <SkillBox title='Backend Dev' markerColor='bg-orange-400' />
+        <div
+          className={styles.skill_boxes + ' w-full 2xl:w-4/5 lg:grid-cols-3'}
+        >
+          <SkillBox
+            title='Software Development'
+            markerColor='bg-pink-400'
+            sections={[
+              {
+                title: '',
+                tagId: 'Languages',
+                content: [
+                  'Experienced in both functional and OOP in: Dart, Python, JavaScript, and TypeScript.'
+                ]
+              },
+              {
+                title: '',
+                tagId: 'Tools',
+                content: ['Git, Github, VS Code, Vim, Docker, Bash.']
+              },
+              {
+                title: '',
+                tagId: 'Methodologies',
+                content: ['Utilize CI/CD and TDD in my development.']
+              }
+            ]}
+          />
+          <SkillBox
+            title='Frontend Dev'
+            markerColor='bg-blue-400'
+            sections={[
+              {
+                title: '',
+                tagId: 'React',
+                content: [
+                  'With more than 2 years of development experience in: HTML, CSS, JS, React and NextJS frameworks.',
+                  '',
+                  'Technologies: NodeJs, React Hooks, Redux, Tailwind, Sass, JestJs.'
+                ]
+              },
+              {
+                title: '',
+                tagId: 'Flutter',
+                content: [
+                  'Bringing over 2 years of development experience in Flutter framework to develop mobile apps and cross-platform solutions.',
+                  '',
+                  'Technologies: Riverpod, Dio, SQLite, Mockito.'
+                ]
+              }
+            ]}
+          />
+          <SkillBox
+            title='Backend Dev'
+            markerColor='bg-orange-400'
+            sections={[
+              {
+                title: '',
+                tagId: 'Know-how',
+                content: [
+                  'Strong Knowledge in networking concepts such as:',
+                  'HTTP, Sockets, OSI model cryptography and security'
+                ]
+              },
+              {
+                title: '',
+                tagId: 'Knowledge',
+                content: [
+                  '• Networking concepts: HTTP, Sockets, OSI model.',
+                  '• Cryptography and security fundamentals.',
+                  '• RESTful API design principles.'
+                ]
+              },
+              {
+                title: '',
+                tagId: 'Tools2',
+                content: [
+                  'Proficient in: SQL, GraphQL, Express.js, Firebase, AWS'
+                ]
+              }
+            ]}
+          />
         </div>
       </div>
     </main>
   )
 }
 
-interface ISkillBox {
+interface SkillBoxProps {
   title: string
   markerColor: string
+  sections?: SkillSectionProps[]
 }
 
-function SkillBox ({ title, markerColor }: ISkillBox) {
+function SkillBox ({ title, markerColor, sections }: SkillBoxProps) {
   return (
     <div className={styles.skill_box}>
       <div className={styles.skill_box_title}>
@@ -37,6 +114,39 @@ function SkillBox ({ title, markerColor }: ISkillBox) {
         ></div>
         <h1>{title}</h1>
       </div>
+      {sections?.map(section => (
+        <SkillSection
+          key={section.tagId}
+          title={section.title}
+          tagId={section.tagId}
+          content={section.content}
+        />
+      ))}
     </div>
+  )
+}
+
+interface SkillSectionProps {
+  title: string
+  tagId: string
+  content: string[]
+}
+
+function SkillSection ({ title, tagId, content }: SkillSectionProps) {
+  return (
+    <section className={styles.skill_section}>
+      <div className={styles.section_title}>
+        <h1>{title}</h1>
+      </div>
+
+      <div
+        className={styles.section_content + ' text-lg max-lg:text-base'}
+        content={`<p id="${tagId}">`}
+      >
+        {content.map((segment, index) =>
+          segment.length > 0 ? <p key={segment + index}>{segment}</p> : <br />
+        )}
+      </div>
+    </section>
   )
 }
