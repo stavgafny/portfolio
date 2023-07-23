de6a20b84feb6bd5e73d379d494d613e14251691
stavgafny <stavgafny@gmail.com>
2023-07-23 21:24:52 +0300

    adding projects section, proper bg gradient color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import About from '@/components/about/About'
 import ContactFooter from '@/components/contact_footer/ContactFooter'
 import Nav, { navHeight } from '@/components/nav/Nav'
 import Skills from '@/components/skills/Skills'
+import Projects from '@/components/projects/Projects'
 
 export default function Main () {
   return (
@@ -11,12 +12,13 @@ export default function Main () {
       <_ScrollScreen id='home' screen={<Home />} />
       <_ScrollScreen id='about' screen={<About />} />
       <_ScrollScreen id='skills' screen={<Skills />} />
+      <_ScrollScreen id='projects' screen={<Projects />} />
       <ContactFooter />
     </div>
   )
 }
 
-function _ScrollScreen ({ screen, id }: { screen: React.ReactElement, id: string }) {
+function _ScrollScreen ({ id, screen }: { id: string, screen: React.ReactElement }) {
   return (
     <section id={id}>
       <div className={navHeight} />
---
+++ src/components/projects/Projects.tsx
@@ -0,0 +1,10 @@
+import styles from './Projects.module.css'
+import AnimateOnScrollObserver from '../shared/AnimateOnScrollObserver'
+
+export default function About () {
+  return (
+    <main className='w-screen min-h-screen flex flex-col max-lg:items-center'>
+        
+    </main>
+  )
+}
